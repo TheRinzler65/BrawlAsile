d0e07f127efe0c676887291cdc4e23577e8e53fe
Rinzler <119778544+TheRinzler65@users.noreply.github.com>
2024-11-28 23:47:07 +0100

    ajout du script qui lance automatiquement l'app

---
+++ start.bat
@@ -0,0 +1,11 @@
+@echo off
+
+:: Ouvrir le premier terminal pour le backend
+start cmd /k "cd /d %~dp0backend && npm run dev"
+
+:: Ouvrir le deuxième terminal pour le frontend
+start cmd /k "cd /d %~dp0frontend && npm run dev"
+
+timeout /t 5
+
+start http://localhost:5174/
